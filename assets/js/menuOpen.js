91c5b870db8370d6697d27ed283b5963f8a42cb0
netsudo <netsudo@users.noreply.github.com>
2026-04-04 17:49:18 -0700

    Add terminal animations

--- assets/js/menuOpen.js
+++ assets/js/menuOpen.js
@@ -1,6 +1,9 @@
 // Toggle the menu open and close when on mobile
 export default function menuOpen() {
     const burgerButton = document.querySelector('.gh-burger');
+    if (!burgerButton) {
+        return;
+    }
     burgerButton.addEventListener('click', function () {
         document.body.classList.toggle('gh-head-open');
     });
